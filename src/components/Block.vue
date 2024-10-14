1375c7520f6857a1157bdcb8ca85ba5076ee743d
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-10-13 22:07:14 -0300

    completed the reaction timer

---
+++ src/components/Block.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    // console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true; // showBlock value is changed!!!
+      // console.log(this.delay);
+      this.startTimer();
+    }, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+  // whenever data is changed, updated() is called
+  // updated() {
+  //   console.log("component updated");
+  // },
+  // unmounted() {
+  //   console.log("unmounted");
+  // },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto; /*top - bot*/
+}
+</style>
